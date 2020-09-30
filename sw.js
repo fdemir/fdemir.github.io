037c1da500235fe28bba01e982b87e83626b3dd4
frndmr <48316033+frndmr@users.noreply.github.com>
2020-09-30 07:17:07 +0000

    Deploying to gh-pages from  @ 3e8a4168672f6ba3e6d30fb6200df85571d9697c 🚀

--- sw.js
+++ sw.js
@@ -28,5 +28,5 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('/frndmr/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/app/'), new workbox.strategies.CacheFirst ({}), 'GET')
 workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
