e2221ec7fd32365d8e5b5d21e260f6720426fa7a
Furkan Demir <frndemir@gmail.com>
2021-03-13 02:33:18 +0200

    fix auto import components issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,12 @@ export default {
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
    */
-  components: true,
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
   /*
    ** Nuxt.js dev-modules
    */
