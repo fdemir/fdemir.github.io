ed20a570b62e36f015fbe4982e19388d8bda0bf2
Furkan Demir <frndemir@gmail.com>
2021-03-13 02:33:24 +0200

    add 404 page demo

---
+++ layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="error-page">
+    <img src="~assets/images/404.png" height="500" alt="" />
+  </div>
+</template>
+
+<style lang="scss" scope>
+.error-page {
+  height: calc(100vh - 90px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
